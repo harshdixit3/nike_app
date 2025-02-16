2d6102f4b56d5308fa8df890b8bf239086933f7b
Harsh Dixit <143201960+harshdixit3@users.noreply.github.com>
2025-02-16 20:11:45 +0530

    Completed the SuperQuality section and Started the Services Section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,8 @@
+import { services } from "../constants";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container justify-center flex-wrap gap-9"></section>
+  );
 };
 
 export default Services;
